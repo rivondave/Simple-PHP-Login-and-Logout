57051bdfbe8a4f18b6593b4c57a4d015e10e9695
David Erivona <74568105+rivondave@users.noreply.github.com>
2023-04-29 14:53:11 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,7 @@
+<?php
+$conn = mysqli_connect("hostname","username","password","database"); //hostname, username, password, database
 
+if(!$conn){
+    echo 'This is not connected to the database';
+}
+?>
